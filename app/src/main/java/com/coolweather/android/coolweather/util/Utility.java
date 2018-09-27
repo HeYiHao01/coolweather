e4ba83e68b8fc7cbdcb72ef143f112945e50285e
Hyh <hyhlovec@gmail.com>
2018-09-27 09:05:55 +0800

    加入显示天气信息

--- app/src/main/java/com/coolweather/android/coolweather/util/Utility.java
+++ app/src/main/java/com/coolweather/android/coolweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.coolweather.android.coolweather.db.City;
 import com.coolweather.android.coolweather.db.County;
 import com.coolweather.android.coolweather.db.Province;
+import com.coolweather.android.coolweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -69,5 +71,17 @@ public class Utility {
             }
         }
         return false;
+    }
+    //JSON数据解析成weather实体类
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
